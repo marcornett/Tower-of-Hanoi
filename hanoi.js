4950f14c392c14800bfdc8c43a54cd7ab49c5f68
Marcel Cornett <marcornett@gmail.com>
2020-12-13 12:47:55 -0500

    Renamed 'pole' variable to 'tower.' Removed unneccesary comments and spaces.

--- hanoi.js
+++ hanoi.js
@@ -1,25 +1,20 @@
-// Poles
-const pole1 = document.querySelector('#pole1')
-const pole2 = document.querySelector('#pole2')
-const pole3 = document.querySelector('#pole3')
+const tower1 = document.querySelector('#tower1')
+const tower2 = document.querySelector('#tower2')
+const tower3 = document.querySelector('#tower3')
 
 // Mode
 let clickMode = "pickUp"
 let selectedDiskNode
 
 const selectTower = function (event) {
-    if ( clickMode === "pickUp" ) {
+    if (clickMode === "pickUp") {
         const selectedTower = event.currentTarget;
-        console.log(event.currentTarget);
-        console.log(clickMode)
         const selectedDisk = selectedTower.lastElementChild;
-        console.log(selectedDisk);
         selectedTower.appendChild(selectedDisk)
         selectedDiskNode = selectedDisk;
         clickMode = "drop"
     }
     else {
-        console.log(clickMode)
         const dropTower = event.currentTarget;
         if (dropTower.childElementCount === 0) {
             dropTower.appendChild(selectedDiskNode);
@@ -29,15 +24,14 @@ const selectTower = function (event) {
             if (selectedDiskNode.dataset.size < topDiskDropTower.dataset.size) {
                 dropTower.appendChild(selectedDiskNode);
             }
-
         }
         clickMode = "pickUp"
     }
-    // for win
-    if (pole2.childElementCount === 4 || pole3.childElementCount === 4) {
+    // Check for Win
+    if (tower2.childElementCount === 4 || tower3.childElementCount === 4) {
         alert("YOU WON!")
     }
 }
-pole1.addEventListener('click', selectTower)
-pole2.addEventListener('click', selectTower)
-pole3.addEventListener('click', selectTower)
+tower1.addEventListener('click', selectTower)
+tower2.addEventListener('click', selectTower)
+tower3.addEventListener('click', selectTower)
